7b26645309c9a76b777fa72c75363eff03707742
readeral <alan@readeral.com>
2020-07-08 21:21:21 +1000

    Initial Sanity commit

---
+++ sanity/dashboardconfig.js
@@ -0,0 +1,19 @@
+
+import {feedSettings} from './dashboardfeed';
+
+export default {
+  widgets: [
+    {
+      name: 'feed-widget',
+      options: feedSettings
+    },
+    {
+      name: 'structure-menu',
+      layout: {width: 'large', height: 'auto'}
+    },
+    {
+      name: 's3upload',
+      layout: {width: 'medium', height: 'auto'}
+    }
+  ]
+};
